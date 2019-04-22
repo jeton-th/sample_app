39d26aa6d1c9ad76ef50af97f54c0b364e3ad248
Jeton Thaçi <jetonthaci@outlook.com>
2019-04-22 15:20:14 +0200

    finish activation chapter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_141018) do
+ActiveRecord::Schema.define(version: 2019_04_22_094124) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_04_18_141018) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
