190544b78c3353771c3f1169db7faee3fa1bba93
Jeton Thaçi <jetonthaci@outlook.com>
2019-10-14 12:39:37 +0200

    bundle update

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title,         'Ruby on Rails Tutorial Sample App'
-    assert_equal full_title(title), "#{title} | Ruby on Rails Tutorial Sample App"
+  test 'full title helper' do
+    assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
   end
 end
